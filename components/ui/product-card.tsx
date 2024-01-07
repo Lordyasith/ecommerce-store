7686e376a8d38cb0c376042333f09f2a9dca01f0
sanjayguwaju <sanjayguwaju@gmail.com>
2024-01-07 18:48:50 +0545

    feat: product preview modal component store

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,11 +2,15 @@
 
 import Image from "next/image";
 import { Expand, ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
-import { useRouter } from "next/navigation";
+import usePreviewModal from "@/hooks/use-preview-modal";
+
+
 
 interface ProductCard {
     data: Product;
@@ -15,12 +19,19 @@ interface ProductCard {
 const ProductCard:React.FC<ProductCard> = ({
     data
 }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
   }
   
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
+  }
+
   return (
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* Images and Actions */}
@@ -34,7 +45,7 @@ const ProductCard:React.FC<ProductCard> = ({
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
             <div className="flex gap-x-6 justify-center">
                 <IconButton 
-                  onClick={()=> {}}
+                  onClick={onPreview}
                   icon={<Expand size={20} className="text-gray-600" />}
                 />
                 <IconButton 
---
+++ providers/modal-provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import PreviewModal from "@/components/preview-modal";
+
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+     
+    useEffect(()=> {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted) {
+        return null;
+    }
+
+  return (
+    <div>
+        <PreviewModal/>
+    </div>
+  )
+}
+
+export default ModalProvider;
